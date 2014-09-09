358c8ba984c5e09af48ee3f81146c588877e713d
walter4dev <github@amifunny.com>
2014-09-09 13:04:40 -0700

    Removed dependency on Company Files for user info
    Previously the code expected https://api.myob.com/accountright/ to return a list of Company Files which would be used to extract User info. Not all accounts have Company Files and this was breaking. The code no longer breaks for accounts with no Company Files.

--- lib/omniauth/strategies/myob.rb
+++ lib/omniauth/strategies/myob.rb
@@ -16,12 +16,14 @@ module OmniAuth
         'scope' => 'CompanyFile'
       }
 
-      uid { raw_info[0]['Id'] }
+      uid {
+        get_from_raw('Id')
+      }
 
       info do
         {
-          'name'     => raw_info[0]['Name'].to_s,
-          'uri'      => raw_info[0]['Uri'].to_s,
+          'name'     => get_from_raw('Name'),
+          'uri'      => get_from_raw('Uri')
         }
       end
 
@@ -29,6 +31,14 @@ module OmniAuth
         { :raw_info => raw_info }
       end
 
+      def get_from_raw(param)
+        if raw_info.length > 0
+          return raw_info[0][param].to_s
+        else
+          return ""
+        end
+      end
+      
       def raw_info
         @raw_info ||= MultiJson.load(access_token.get('https://api.myob.com/accountright/', {:headers => headers}).body)
       end
